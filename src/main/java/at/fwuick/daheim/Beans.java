3edf4c78812cf168602ba699c6298904e9bab28b
SchneiderL <SchneiderL@ATVIE168.compax.intern>
2017-08-07 16:19:12 +0200

    update for heroku

--- src/main/java/at/fwuick/daheim/Beans.java
+++ src/main/java/at/fwuick/daheim/Beans.java
@@ -1,29 +1,50 @@
 package at.fwuick.daheim;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.security.SecureRandom;
 
+import javax.sql.DataSource;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Beans {
-	@Bean
-	public SecureRandom secureRandom(){
-		return new SecureRandom();
-	}
-	
-	@Bean
-	public Validator validator(){
-		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-		return factory.getValidator();
-	}
-	
-	@Bean
-	public Logger logger(){
-		return Logger.getLogger(DaheimBackendApplication.class);
-	}
+  @Bean
+  public SecureRandom secureRandom() {
+    return new SecureRandom();
+  }
+
+  @Bean
+  public Validator validator() {
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    return factory.getValidator();
+  }
+
+  @Bean
+  public Logger logger() {
+    return Logger.getLogger(DaheimBackendApplication.class);
+  }
+
+  @Bean
+  public DataSource dataSource() throws URISyntaxException {
+    URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+
+    String username = dbUri.getUserInfo().split(":")[0];
+    String password = dbUri.getUserInfo().split(":")[1];
+    String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+    BasicDataSource basicDataSource = new BasicDataSource();
+    basicDataSource.setUrl(dbUrl);
+    basicDataSource.setUsername(username);
+    basicDataSource.setPassword(password);
+
+    return basicDataSource;
+  }
 }
--- src/main/java/at/fwuick/daheim/dao/UserDao.java
+++ src/main/java/at/fwuick/daheim/dao/UserDao.java
@@ -1,19 +1,15 @@
 package at.fwuick.daheim.dao;
 
-import static at.fwuick.daheim.utils.DaheimUtils.data;
 import static at.fwuick.daheim.utils.QueryUtils.select;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import at.fwuick.daheim.DaheimException;
 import at.fwuick.daheim.DaheimExceptionSupplier;
-import at.fwuick.daheim.DaheimExceptionSupplier.Errors;
 import at.fwuick.daheim.model.User;
 import at.fwuick.daheim.model.UserIdentity;
 
@@ -31,6 +27,7 @@ public class UserDao {
   public void insert(User user) {
     jdbcTemplate.update("INSERT INTO USERS (name, uuid) VALUES (?, ?)", new Object[] { user.getName(), user.getUuid() });
     user.setId(jdbcTemplate.queryForObject("select max(id) from users", Long.class));
+    jdbcTemplate.update("insert into user_status (user, status) values (?, 1)", new Object[] { user.getId() });
   }
 
   private RowMapper<User> baseUserMapper = (rs, rowNum) -> {
@@ -42,9 +39,6 @@ public class UserDao {
     user.setStatus(rs.getLong("status"));
     return user;
   };
-
-
-
 
   public void updateHome(User user) {
     jdbcTemplate.update("update users set home = ? where id = ?", new Object[] { user.getHome(), user.getId() });
@@ -58,6 +52,6 @@ public class UserDao {
   }
 
   public User get(UserIdentity id) {
-	  return jdbcTemplate.queryForObject(select("*", TABLE_NAME).where(id.getName()), new Object[] { id.getValue() }, baseUserMapper);
+    return jdbcTemplate.queryForObject(select("*", TABLE_NAME).where(id.getName()), new Object[] { id.getValue() }, baseUserMapper);
   }
 }
